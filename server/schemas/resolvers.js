76f8b7c2f8eeb0789f5db445648e4f7c1f03a3bb
[eigenvalue000] <ghkegel@gmail.com>
2021-12-03 20:08:44 -0800

    Corrected resolver map.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,1 +1,18 @@
-console.log('Hello world');
+// const { AuthenticationError } = require('apollo-server-express');
+const { Stock } = require('../models');
+// const { signToken } = require('../utils/auth');
+
+
+const resolvers = {
+  Query: {
+    numberSix() {
+      return 6;
+    },
+    numberSeven() {
+      return 7;
+    }
+  },
+  Mutation: {}
+};
+
+module.exports = resolvers;
