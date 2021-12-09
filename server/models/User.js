39d6b66f8d1da636bf3e14d680b2f3fce3917c46
theDaveB <davebloisesquire@gmail.com>
2021-12-08 20:05:11 -0800

    Adding salt rounds to user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
 const { Schema } = mongoose;
 
 
@@ -25,6 +25,19 @@ const userSchema = new Schema({
     allowNull: true
   }
 });
+
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
 
