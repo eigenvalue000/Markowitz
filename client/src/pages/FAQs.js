e1f4d7e4243e24c6861caf8b7f1ab19febef691a
Ayele Lintamo <lashaaye@gmail.com>
2021-12-10 13:57:26 -0800

    changing faqs code

--- client/src/pages/FAQs.js
+++ client/src/pages/FAQs.js
@@ -1,17 +1,66 @@
+import React, { useState } from "react";
 
-import React, { useState } from 'react'
+const FORM_ENDPOINT = ""; 
+// TODO - fill on the later step
 
-const Contact = () => {
+const ContactForm = () => {
+  const [submitted, setSubmitted] = useState(false);
+  const handleSubmit = () => {
+    setSubmitted(true);
+  };
+
+  if (submitted) {
+    return (
+      <>
+        <div className="text-2xl">Thank you!</div>
+        <div className="text-md">We'll be in touch soon.</div>
+      </>
+    );
+  }
+
   return (
-      <div id="contact-form">
-          <input type="text" placeholder="Your Name" />
-          <input type="email" placeholder="Your email address" />
-          <textarea placeholder="Your question"></textarea>
-          <button>Send Message</button>
-          <span>Thank you for your message, we will be in touch in no time!</span>
+    <form
+      action={FORM_ENDPOINT}
+      onSubmit={handleSubmit}
+      method="POST"
+      target="_blank"
+    >
+      <div className="mb-3 pt-0">
+        <input
+          type="text"
+          placeholder="Your name"
+          name="name"
+          className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+          required
+        />
       </div>
-  
-  
- );
+      <div className="mb-3 pt-0">
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+          required
+        />
+      </div>
+      <div className="mb-3 pt-0">
+        <textarea
+          placeholder="Your message"
+          name="message"
+          className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+          required
+        />
+      </div>
+      <div className="mb-3 pt-0">
+        <button
+          className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+          type="submit"
+        >
+          Send a message
+        </button>
+      </div>
+    </form>
+  );
 };
-export default Contact;
+
+export default ContactForm;
